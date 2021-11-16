21e586e08c8f57890771a3236c62acb31140c7e6
NnelisS <78425965+NnelisS@users.noreply.github.com>
2021-11-16 16:17:24 +0100

    Timer

--- Endless Runner/Assets/Scripts/GameManager.cs
+++ Endless Runner/Assets/Scripts/GameManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI timerText;
+    public bool playing = false;
+    private float timer;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (playing)
+        {
+            timer += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timer / 60F);
+            int seconds = Mathf.FloorToInt(timer % 60F);
+            int milliseconds = Mathf.FloorToInt((timer * 100F) % 100F);
+            timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+        }
     }
 }
