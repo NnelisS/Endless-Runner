80d2df326d171ecee1d028d02d7087895548db14
robbin3105 <78424372+robbin3105@users.noreply.github.com>
2021-11-19 10:37:06 +0100

    playermovement rotate nee

--- Endless Runner/Assets/Scripts/PushPull.cs
+++ Endless Runner/Assets/Scripts/PushPull.cs
@@ -9,30 +9,27 @@ public class PushPull : MonoBehaviour
     private float distance = 1f;
     [SerializeField]
     private Vector3 offset;
-
-    public LayerMask box_mask;
+    [SerializeField]
+    private LayerMask box_mask;
 
     private PlayerMovement movement_script;
-
     private Rigidbody rb;
 
     private GameObject box;
-
     private bool is_pulling;
 
-    // Start is called before the first frame update
     void Start()
     {
         movement_script = gameObject.GetComponent<PlayerMovement>();
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
+
         bool is_hit = Physics.Raycast(transform.position, Vector3.right * movement_script.x_dir, out hit, distance, box_mask);
-        Debug.Log(is_hit);
+
         if (is_pulling)
         {
             if (box != null) 
@@ -44,11 +41,10 @@ public class PushPull : MonoBehaviour
 
         if(is_hit && Input.GetKeyDown(KeyCode.LeftShift))
         {
-
             box = hit.collider.gameObject;
-            
             SwitchPushPullState();
-        }else if(is_pulling && Input.GetKeyUp(KeyCode.LeftShift))
+        }
+        else if(is_pulling && Input.GetKeyUp(KeyCode.LeftShift))
         {
             SwitchPushPullState();
             box = null;
@@ -62,7 +58,6 @@ public class PushPull : MonoBehaviour
 
         is_pulling = !is_pulling;
         movement_script.ChangeSpeed(is_pulling);
-
     }
 
 
