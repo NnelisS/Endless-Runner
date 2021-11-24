8abd3d3013c3a5302e17d574af37404bcd4fc63b
robbin3105 <78424372+robbin3105@users.noreply.github.com>
2021-11-24 13:18:23 +0100

    PUSH

--- Endless Runner/Assets/Scripts/PlayerMovement.cs
+++ Endless Runner/Assets/Scripts/PlayerMovement.cs
@@ -106,12 +106,15 @@ public class PlayerMovement : MonoBehaviour
     IEnumerator Slide()
     {
         float current_direction = facing_right ? -1 : 1;
-        cc.height = 1.5f;
+        cc.center = new Vector3(0, -0.76f, 0);
+        cc.height = 1f;
+        yield return new WaitForSeconds(0.1f);
         rb.AddForce(Vector2.right * current_direction * slide_force, ForceMode.Impulse);
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(0.6f);
         can_jump = true;
         can_rotate = true;
         can_move = true;
+        cc.center = new Vector3(0, 0, 0);
         cc.height = 2.5f;
         yield return new WaitForSeconds(0.6f);
         is_sliding = false;
